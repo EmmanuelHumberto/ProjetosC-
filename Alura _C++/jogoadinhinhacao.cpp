2018ed689602767302353cd1f1a8bee787d3f03b
ss <setrotec@outlook.com>
2021-04-05 00:13:20 -0300

    Desenvolvendo o jogo da adivinhação

--- Alura _C++/jogoadinhinhacao.cpp
+++ Alura _C++/jogoadinhinhacao.cpp
@@ -2,8 +2,10 @@
  #include<iostream>
  using namespace std;
 
+   
+     int main() {
 
-  int main() {
+    const int NUMERO_SECRETO = 666;
 
     /*função cout, entrada e saida [std::] bilioteca padrão in/out.
      * função endl, para quebra de linha, pode ser usando também [\n] do C
@@ -13,13 +15,29 @@
      std::cout << "*********************************************\n";
 
 
-    /*
-     * Outra forma de se escrever a função std::cout é declarando [using namespace std]
-        assim o [std::] antes da função não precisa ser escrtito 
-     */
-     cout << "*********************************************"<<endl;
-	 cout << "*  Bem vindo! Ao nosso jogo da Adivinhacao! *"<<endl;
-     cout << "*********************************************\n";
+     int bicudo;
+     cout << " Qual o seu bicudo?";
+     cin >> bicudo;
+     cout << "O bicudo dados foi" << bicudo << endl;
+
+     bool acertou = bicudo == NUMERO_SECRETO;
+     bool maior = bicudo > NUMERO_SECRETO;
+
+     if(acertou){
+
+         cout << "Parabens você deu um bicudo certeiro" << endl;
+
+     }
+     else if(maior){
+
+         cout << "Seu bicudo foi MAIOR que o numero Secreto" << endl;
+
+     }
+     else{
+
+          cout << "Seu bicudo foi MENOR que o numero Secreto" << endl;
+
+     }
    	
-   	
-  }
+
+    }
