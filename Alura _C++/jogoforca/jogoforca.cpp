f3b973e8fbc8529c0ac3de363a6ccb2423461e11
ss <setrotec@outlook.com>
2021-04-07 14:21:07 -0300

    Fatorando o codigo jogoforca

--- Alura _C++/jogoforca/jogoforca.cpp
+++ Alura _C++/jogoforca/jogoforca.cpp
@@ -1,71 +1,126 @@
- #include<iostream>
- #include<string>
- #include<cstdlib>
- #include<ctime>
+#include<iostream>
+#include<string>
+#include<cstdlib>
+#include<ctime>
 #include<map>
 #include<vector>
+using namespace std;
+
+void cabecalho(){
+
+printf("************************\n");
+printf("* Ola! Eu sou a forca. *\n");
+printf("************************\n");
+printf("\n");
 
 
- using namespace std;
+printf("Vamos comerçar?\n");
+printf("\n");
 
- const string PALAVRA_SECRETA = "MELANCIA";
- map<char, bool> bicudou;
- vector<char>bicudas_erradas;
+}
 
- bool letra_existe(char bicu){ 
-     //for iterado
+const string PALAVRA_SECRETA = "MELANCIA";
+map<char, bool> bicudou;
+vector<char>bicudas_erradas;
+
+bool letra_existe(char bicu){ 
+    //for iterado
     for(char letra : PALAVRA_SECRETA){
         if(bicu == letra){
         return true;
          }
      }
-
      return false;
+ }
+
+bool nao_acertou(){
+
+    for(char letra : PALAVRA_SECRETA){
+
+        if(!bicudou[letra]){
+        
+        return true;
+         }
      }
+     return false;
+ }  
 
- int main(){
+bool nao_inforcou(){
 
-    bool acertou = false;
-    bool inforcou = false;
+    return bicudas_erradas.size() < 5;
+ }     
 
-     while (!acertou && !inforcou){
+ void bicos_errados(){
 
-        cout << "bicudos errados: "; 
-        for(char letra : bicudas_erradas){
+    cout << "bicudos errados: "; 
+    for(char letra : bicudas_erradas){
              cout << letra << " "; 
-         }
-
-        cout << "\n";
-
-        for(char letra : PALAVRA_SECRETA){
-            if (bicudou [letra]){
-                cout << letra << " ";
-             }
-             else{
-                cout << "_ ";
-             }
-
-         }
-
-         cout << "\n";
-         cout << "\nBicuda Aí!  ";
-         char bicu;
-         cin >> bicu;
-
-         bicudou[bicu] = true;
-
-         if(letra_existe(bicu)){
-
-             cout << "Voce Acertou! Deu um bicu correto"<< endl; 
-
-         }else{
-
-             cout << "Voce ERROU! Deu um bicu pra longe"<< endl; 
-             bicudas_erradas.push_back(bicu);
-
-         }
-         cout << endl;
      }
+
+     cout << endl;
+
+ }
+
+ void desenha_palavra(){
+
+for(char letra : PALAVRA_SECRETA){
+    if (bicudou [letra]){
+        cout << letra << " ";
+        }
+        else{
+        cout << "_ ";
+         }
+     }
+    cout << endl;
+ }
+
+ void bicuda(){
+
+    cout << "\n";
+    cout << "\nBicuda Aí!  ";
+    char bicu;
+    cin >> bicu;
+
+    bicudou[bicu] = true;
+
+    if(letra_existe(bicu)){
+
+        cout << "Voce Acertou! Deu um bicu correto"<< endl; 
+
+    }else{
+
+        cout << "Voce ERROU! Deu um bicu pra longe"<< endl; 
+        bicudas_erradas.push_back(bicu);
+
+    }
+    cout << endl;
+ }
+
+
+int main(){
+
+     cabecalho();
+
+     while (nao_acertou() && nao_inforcou()){
+
+         bicos_errados();
+         desenha_palavra();
+         bicuda();
+
+
+        
+     }
+
+      cout << "Fim do jogo!" << endl;
+      cout << "A palavra secreta é: " << PALAVRA_SECRETA << endl;
+      if(nao_acertou()){
+
+           cout << "Voce perdeu o jogo!" << endl;
+      }else{
+
+           cout << "Parabens! Você acertou a palavra secreta!" << endl;
+        
+      }
       
  }
 
