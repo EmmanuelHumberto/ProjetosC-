23e3a8a4be31018dbe41ca80f39e9655f980a3db
ss <setrotec@outlook.com>
2021-04-07 16:59:46 -0300

    Implementando sorteio de palavras no jogo da forca

--- Alura _C++/jogoforca/jogoforca.cpp
+++ Alura _C++/jogoforca/jogoforca.cpp
@@ -21,13 +21,13 @@ printf("\n");
 
 }
 
-const string PALAVRA_SECRETA = "MELANCIA";
+string palavra_secreta = "MELANCIA";
 map<char, bool> bicudou;
 vector<char>bicudas_erradas;
 
 bool letra_existe(char bicu){ 
     //for iterado
-    for(char letra : PALAVRA_SECRETA){
+    for(char letra : palavra_secreta){
         if(bicu == letra){
         return true;
          }
@@ -37,10 +37,8 @@ bool letra_existe(char bicu){
 
 bool nao_acertou(){
 
-    for(char letra : PALAVRA_SECRETA){
-
+    for(char letra : palavra_secreta){
         if(!bicudou[letra]){
-        
         return true;
          }
      }
@@ -60,12 +58,11 @@ bool nao_inforcou(){
      }
 
      cout << endl;
-
  }
 
  void desenha_palavra(){
 
-for(char letra : PALAVRA_SECRETA){
+for(char letra : palavra_secreta){
     if (bicudou [letra]){
         cout << letra << " ";
         }
@@ -82,40 +79,42 @@ for(char letra : PALAVRA_SECRETA){
     cout << "\nBicuda Aí!  ";
     char bicu;
     cin >> bicu;
-
     bicudou[bicu] = true;
 
     if(letra_existe(bicu)){
-
         cout << "Você Acertou! Deu um bicu correto"<< endl; 
 
     }else{
-
         cout << "Voce ERROU! Deu um bicu pra longe"<< endl; 
         bicudas_erradas.push_back(bicu);
-
     }
     cout << endl;
- }
+ }  
 
-void le_arquivo(){
+vector<string> le_arquivo(){
 
     ifstream arquivo;
     arquivo.open("palavras_forca.txt");
-
     int qte_palavras;
     arquivo >> qte_palavras;
+    vector<string> palavras_do_arquivo;
 
-    cout << "O arquivo possui " << qte_palavras << "palavras." << endl;
 
     for(int i = 0; i < qte_palavras; i++){
         string palavra_lida;
         arquivo >> palavra_lida;
+        palavras_do_arquivo.push_back(palavra_lida);
+     }
+     return palavras_do_arquivo;
+ }
 
-        cout << "na linha " << i << " : " << palavra_lida << endl;
+void sorteia_palavra_secreta(){
 
+    vector<string> palavra_sorteada  = le_arquivo();
+    srand(time(NULL));
+    int indice_sorteado = rand() % palavra_sorteada.size();
+    palavra_secreta = palavra_sorteada[indice_sorteado];
 
-    }
 
 
 }
@@ -123,8 +122,8 @@ void le_arquivo(){
 int main(){
 
      cabecalho();
-
      le_arquivo();  
+     sorteia_palavra_secreta();
 
      while (nao_acertou() && nao_inforcou()){
 
@@ -134,13 +133,11 @@ int main(){
      }
 
       cout << "Fim do jogo!" << endl;
-      cout << "A palavra secreta é: " << PALAVRA_SECRETA << endl;
+      cout << "A palavra secreta é: " << palavra_secreta << endl;
       if(nao_acertou()){
-
            cout << "Voce perdeu o jogo!" << endl;
 
       }else{
-
            cout << "Parabens! Você acertou a palavra secreta!" << endl;
         
       }
