a2cd90117b707ab7beeeee8c21672f8225aeed2e
ss <setrotec@outlook.com>
2021-04-07 01:47:40 -0300

    Implementando contagem de bicos errados

--- Alura _C++/jogoforca/jogoforca.cpp
+++ Alura _C++/jogoforca/jogoforca.cpp
@@ -2,9 +2,15 @@
  #include<string>
  #include<cstdlib>
  #include<ctime>
+#include<map>
+#include<vector>
+
+
  using namespace std;
 
-  const string PALAVRA_SECRETA = "MELANCIA";
+ const string PALAVRA_SECRETA = "MELANCIA";
+ map<char, bool> bicudou;
+ vector<char>bicudas_erradas;
 
  bool letra_existe(char bicu){ 
      //for iterado
@@ -13,20 +19,40 @@
         return true;
          }
      }
+
      return false;
- }
+     }
 
  int main(){
 
-     cout << PALAVRA_SECRETA << endl; 
+    bool acertou = false;
+    bool inforcou = false;
 
-    bool nao_acertou = true;
-    bool nao_inforcou = true;
+     while (!acertou && !inforcou){
 
-     while (nao_acertou && nao_acertou){
+        cout << "bicudos errados: "; 
+        for(char letra : bicudas_erradas){
+             cout << letra << " "; 
+         }
 
+        cout << "\n";
+
+        for(char letra : PALAVRA_SECRETA){
+            if (bicudou [letra]){
+                cout << letra << " ";
+             }
+             else{
+                cout << "_ ";
+             }
+
+         }
+
+         cout << "\n";
+         cout << "\nBicuda Aí!  ";
          char bicu;
          cin >> bicu;
+
+         bicudou[bicu] = true;
 
          if(letra_existe(bicu)){
 
@@ -35,8 +61,11 @@
          }else{
 
              cout << "Voce ERROU! Deu um bicu pra longe"<< endl; 
+             bicudas_erradas.push_back(bicu);
 
          }
+         cout << endl;
      }
+      
  }
 
